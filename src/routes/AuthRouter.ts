d18543339a21146f8e64a77efa2e0236a3b42881
i5x64BIT <vaintomer@gmail.com>
2023-12-19 19:06:37 +0200

    Add token timeout and Modify error messege on duplicate token

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 
 const secret = process.env.TOKEN_SECRET || "pink-turtle";
 const createToken = (payload: any, timeout: string | number = 900) => {
-  const options: jwt.SignOptions = { algorithm: "HS256", expiresIn: "15m" };
+  const options: jwt.SignOptions = { algorithm: "HS256", expiresIn: timeout };
   const token = jwt.sign({ ...payload }, secret, options);
   return token;
 };
@@ -22,14 +22,20 @@ router.put("/login", (req, res, next) => {
       return next(new AuthanticateError());
     }
     const { _id, email, role, address, phone_num, cart, receipts } = user;
-    const accessToken = createToken({
-      _id,
-      role,
-    });
-    const refreshToken = createToken({
-      _id,
-      role,
-    });
+    const accessToken = createToken(
+      {
+        _id,
+        role,
+      },
+      "10m"
+    );
+    const refreshToken = createToken(
+      {
+        _id,
+        role,
+      },
+      "1d"
+    );
     user.refreshToken = refreshToken;
     user.save();
     res.cookie("token", refreshToken, {
@@ -54,74 +60,77 @@ router.put("/login", (req, res, next) => {
   });
 });
 router.put("/refresh", async (req, res, next) => {
-  if(req.cookies?.token){
-  try {
-    const decoded = jwt.verify(
-      req.cookies.token,
-      process.env.TOKEN_SECRET
-    ) as JwtPayload;
-    const user = await UserModel.findOne({ refreshToken: req.cookies.token });
-    if (!user) {
-      // Duplicate use for key
-      const hackedUser = await UserModel.findById(decoded._id);
-      hackedUser.refreshToken = "";
-      hackedUser.save();
-      return next(
-        new AuthanticateError("Something wen't wrong, please re-login")
-      );
-    } else {
-      const newRefreshToken = createToken(
-        {
+  if (req.cookies?.token) {
+    try {
+      const decoded = jwt.verify(
+        req.cookies.token,
+        process.env.TOKEN_SECRET
+      ) as JwtPayload;
+      const user = await UserModel.findOne({ refreshToken: req.cookies.token });
+      if (!user) {
+        // Duplicate use for key
+        const hackedUser = await UserModel.findById(decoded._id);
+        hackedUser.refreshToken = "";
+        hackedUser.save();
+        return next(
+          new AuthanticateError("Found Duplicate on " + hackedUser.email)
+        );
+      } else {
+        const newRefreshToken = createToken(
+          {
+            _id: user._id,
+            role: user.role,
+          },
+          "1d"
+        );
+        const newAccessToken = createToken({
           _id: user._id,
           role: user.role,
-        },
-        "1d"
-      );
-      const newAccessToken = createToken({
-        _id: user._id,
-        role: user.role,
-      });
-      user.refreshToken = newRefreshToken;
-      user.save();
-      res.cookie("token", newRefreshToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-      });
-      res.status(200).send({
-        messege: "OK",
-        token: newAccessToken,
-      });
+        });
+        user.refreshToken = newRefreshToken;
+        user.save();
+        res.cookie("token", newRefreshToken, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        });
+        res.status(200).send({
+          messege: "OK",
+          token: newAccessToken,
+        });
+      }
+    } catch (error) {
+      // jwt error
+      const decoded = jwt.decode(req.cookies?.token) as JwtPayload;
+      if (!decoded) {
+        return next(new AuthoriseError("Bad Token or missing Token"));
+      } else {
+        // Refresh Token Expired
+        const expiredUser = await UserModel.findById(decoded._id);
+        expiredUser.refreshToken = "";
+        expiredUser.save();
+        return next(new AuthanticateError("Token Expired"));
+      }
     }
-  } catch (error) {
-    // jwt error
-    const decoded = jwt.decode(req.cookies?.token) as JwtPayload;
-    if (!decoded) {
-      return next(new AuthoriseError("Bad Token or missing Token"));
-    } else {
-      // Refresh Token Expired
-      const expiredUser = await UserModel.findById(decoded._id);
-      expiredUser.refreshToken = "";
-      expiredUser.save();
-      return next(new AuthanticateError("Token Expired"));
-    }
+  } else {
+    console.log("Missing token request");
+    res.status(401).json({
+      messege: "Missing refresh token",
+    });
   }
-} else{
-  console.log("Missing token request")
-  res.status(401).json({
-    messege: "Missing refresh token"
-  })
-}
 });
 router.put("/logout", async (req, res, next) => {
   try {
-    const decoded : any = jwt.verify(req.cookies.token, process.env.TOKEN_SECRET);
+    const decoded: any = jwt.verify(
+      req.cookies.token,
+      process.env.TOKEN_SECRET
+    );
     const user = await UserModel.findById(decoded._id);
     user.refreshToken = "";
     user.save();
     res.status(200).json({
-      messege: "Logged Out successfully"
-    })
+      messege: "Logged Out successfully",
+    });
   } catch (error) {
     //JWT is not valid
     res.status(401).json({
